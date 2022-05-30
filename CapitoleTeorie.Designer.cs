631440bbb3dc967d0253f23740031823ff51581f
Sebastian2110 <103482734+Sebastian2110@users.noreply.github.com>
2022-05-30 17:06:48 +0300

    nou

--- CapitoleTeorie.Designer.cs
+++ CapitoleTeorie.Designer.cs
@@ -107,12 +107,12 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Gabriola", 12F);
+            this.button1.Font = new System.Drawing.Font("Gabriola", 18F);
             this.button1.Location = new System.Drawing.Point(13, 251);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(471, 112);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Topografie";
+            this.button1.Text = "      Topografie";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
