e356bb22cc45ad7560bc739f8fde302635d4a4d3
Sebastian2110 <103482734+Sebastian2110@users.noreply.github.com>
2022-07-06 12:30:05 +0300

    nou

--- Reflexul.Designer.cs
+++ Reflexul.Designer.cs
@@ -30,16 +30,16 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reflexul));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -57,26 +57,21 @@
             this.panel1.Size = new System.Drawing.Size(1379, 2112);
             this.panel1.TabIndex = 0;
             // 
-            // label2
+            // label9
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Gabriola", 50F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(599, 7);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(278, 155);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Reflexul";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Location = new System.Drawing.Point(3, 1528);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(1367, 234);
+            this.label9.TabIndex = 10;
+            this.label9.Text = resources.GetString("label9.Text");
             // 
-            // label1
+            // label8
             // 
-            this.label1.Location = new System.Drawing.Point(3, 144);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1381, 983);
-            this.label1.TabIndex = 7;
-            this.label1.Text = resources.GetString("label1.Text");
+            this.label8.Location = new System.Drawing.Point(762, 1083);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(622, 434);
+            this.label8.TabIndex = 9;
+            this.label8.Text = resources.GetString("label8.Text");
             // 
             // panel2
             // 
@@ -93,33 +88,15 @@
             this.panel2.Size = new System.Drawing.Size(744, 416);
             this.panel2.TabIndex = 8;
             // 
-            // label3
+            // label7
             // 
-            this.label3.Location = new System.Drawing.Point(246, 6);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(235, 42);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Receptori tactili";
-            // 
-            // label4
-            // 
-            this.label4.Font = new System.Drawing.Font("Times New Roman", 14F);
-            this.label4.Location = new System.Drawing.Point(26, 339);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(128, 59);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Corpusculii Meissner";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label5
-            // 
-            this.label5.Font = new System.Drawing.Font("Times New Roman", 14F);
-            this.label5.Location = new System.Drawing.Point(228, 339);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 59);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Discurile Merkel";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.label7.Location = new System.Drawing.Point(584, 339);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(140, 59);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Corpusculii Vater-Pacini";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label6
             // 
@@ -131,31 +108,54 @@
             this.label6.Text = "Corpusculii Ruffini";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label7
+            // label5
             // 
-            this.label7.Font = new System.Drawing.Font("Times New Roman", 14F);
-            this.label7.Location = new System.Drawing.Point(584, 339);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(140, 59);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "Corpusculii Vater-Pacini";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.label5.Location = new System.Drawing.Point(228, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 59);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Discurile Merkel";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label8
+            // label4
             // 
-            this.label8.Location = new System.Drawing.Point(762, 1083);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(622, 434);
-            this.label8.TabIndex = 9;
-            this.label8.Text = resources.GetString("label8.Text");
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.label4.Location = new System.Drawing.Point(26, 339);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 59);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Corpusculii Meissner";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label9
+            // label3
             // 
-            this.label9.Location = new System.Drawing.Point(3, 1528);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(1367, 234);
-            this.label9.TabIndex = 10;
-            this.label9.Text = resources.GetString("label9.Text");
+            this.label3.Location = new System.Drawing.Point(246, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(235, 42);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Receptori tactili";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1381, 983);
+            this.label1.TabIndex = 7;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Gabriola", 50F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(599, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 155);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Reflexul";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Reflexul
             // 
