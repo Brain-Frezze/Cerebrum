05c73e9c2855b336c0c4d27e9759f0584958be7f
Sebastian2110 <103482734+Sebastian2110@users.noreply.github.com>
2022-07-21 15:15:21 +0300

    zoom imagine

--- Neurotransmiţători.cs
+++ Neurotransmiţători.cs
@@ -16,5 +16,35 @@ namespace Cerebrum
         {
             InitializeComponent();
         }
+
+        Image marire(Image img, Size size)
+        {
+            Bitmap bm = new Bitmap(img, Convert.ToInt32(img.Width*size.Width/2), Convert.ToInt32(img.Height*size.Height/2));
+            Graphics gpu = Graphics.FromImage(bm);
+            gpu.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bm;
+        }
+        PictureBox org;
+        private void Neurotransmiţători_Load(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 6;
+            trackBar1.SmallChange = 1;
+            trackBar1.LargeChange = 1;
+            trackBar1.UseWaitCursor = false;
+
+            this.DoubleBuffered = true;
+            org = new PictureBox();
+            org.Image = pictureBox1.Image;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value != 0)
+            {
+                pictureBox1.Image = null;
+                pictureBox1.Image = marire(org.Image, new Size(trackBar1.Value, trackBar1.Value));
+            }
+        }
     }
 }
