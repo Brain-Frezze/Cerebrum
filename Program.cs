90dc7e35642c5ddc58a648beec4126660ff6c5b6
Sebastian2110 <103482734+Sebastian2110@users.noreply.github.com>
2022-07-25 02:28:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace Cerebrum
             //Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ro");
-            Application.Run(new Login());
+            Application.Run(new StartView());
 
         }
     }
